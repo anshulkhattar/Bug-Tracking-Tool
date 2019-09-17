d642c20ae7181dbc82c61c9ffe0ad8239dbb0ea7
Anshul Khattar <anshul070300@gmail.com>
2019-09-17 22:25:47 +0530

    Commands file added

---
+++ custom_commands.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function testerview(){
+gcc /home/anshul/Bug-Tracking-Tool/Tester_view.c -o testerView
+./testerView
+}
+function testercreate(){
+gcc /home/anshul/Bug-Tracking-Tool/Tester_create.c -o testerCreate
+./testerCreate
+}
