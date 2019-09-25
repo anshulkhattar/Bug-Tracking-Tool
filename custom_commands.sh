01d028629c0667ea6b3c5534d036ac3795baa1f6
Anshul Khattar <anshul070300@gmail.com>
2019-09-26 00:43:28 +0530

    custom command updated

--- custom_commands.sh
+++ custom_commands.sh
@@ -8,3 +8,17 @@ function testercreate(){
 gcc /home/anshul/Bug-Tracking-Tool/Tester_create.c -o testerCreate
 ./testerCreate
 }
+
+function bgtool(){
+    if [ "$1" == "tester" ]
+    then 
+        if [ "$2" == "view" ]
+        then
+            testerview
+        elif [ "$2" == "create" ]
+        then
+            testercreate
+        fi
+    fi
+
+}
